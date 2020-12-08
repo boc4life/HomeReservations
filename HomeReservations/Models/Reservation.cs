35b7c314d428decac622ba93cc0fb8a7f01dd5eb
boc4life <lheandrew@gmail.com>
2020-12-08 00:02:40 -0500

    small front-end changes and source control cleanup

--- HomeReservations/Models/Reservation.cs
+++ HomeReservations/Models/Reservation.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeReservations.Models
 {
     public class Reservation
     {
         public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         public int UnitID { get; set; }
 
